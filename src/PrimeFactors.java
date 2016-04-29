5a872dd502d4e5620e5a2ebdf57ac2cbf20b779b
shion <shion@sg-2.local>
2016-04-29 16:10:09 +0800

    Removed magic number

--- src/PrimeFactors.java
+++ src/PrimeFactors.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
  * Created by shion on 29/4/16.
  */
 public class PrimeFactors {
+    final static int UNIT_NUMBER = 1;
 
     public static void main(String[] args) {
         System.out.println(generate(1));
@@ -13,9 +14,9 @@ public class PrimeFactors {
     public static ArrayList<Integer> generate(int n) {
         ArrayList<Integer> primeList = new ArrayList<Integer>();
 
-        if (n == 1) return primeList;
+        if (n <= UNIT_NUMBER) return primeList;
 
-        for (int currentNumber = 2 ; currentNumber <= n ; currentNumber++) {
+        for (int currentNumber = UNIT_NUMBER + 1 ; currentNumber <= n ; currentNumber++) {
             if (isDivisible(currentNumber, n) && isPrime(currentNumber, primeList)) {
                 primeList.add(currentNumber);
             }
